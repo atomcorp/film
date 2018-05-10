334670b81a079c5e015fac3183e1df06b250e5d0
atomcorp <thomasmaxwellsmith@gmail.com>
2018-05-10 22:50:39 +0100

    dragabble with filter is not going to work

--- src/containers/DraggableList/DraggableList.js
+++ src/containers/DraggableList/DraggableList.js
@@ -8,19 +8,30 @@ import {reorderCollection} from '../../redux/actions/collection-actions';
  * DraggableList using DnD
  */
 class DraggableList extends Component {
-  onDragStart = ({draggableId, type, source}) => {
-  // const type = {
-  //   draggableId: 'DraggableId',
-  //   type: 'TypeId',
-  //   source: 'DraggableLocation',
-  // };
   /**
-  * This hook is optional and therefore does not need to be provided.
-  * It is highly recommended that you use this function to block
-  * updates to all Draggable and Droppable components during a
-  * drag. (See *Best practices for hooks *)
-  */
-                                                 };
+   * Constructor
+   * @param {object} props
+   */
+  constructor(props) {
+    super(props);
+  }
+  /**
+   * On Drag Start
+   * @param {object} start
+   * @param {string} start.draggableId
+   * @param {string} start.TypeId
+   * @param {object} start.source
+   * @param {object} provided
+   */
+  onDragStart = (start, provided) => {
+    /**
+     * This hook is optional and therefore does not need to be provided.
+     * It is highly recommended that you use this function to block
+     * updates to all Draggable and Droppable components during a
+     * drag. (See *Best practices for hooks *)
+     */
+    // console.log(start, provided);
+  };
   onDragUpdate = ({draggableId, type, source}) => {
     //
   };
@@ -30,11 +41,25 @@ class DraggableList extends Component {
    */
   onDragEnd = (DragUpdate, DropReason) => {
     // the only one that is required
+    const trueIndex = this.props.films.findIndex(
+      (film) => DragUpdate.draggableId === film.imdbID
+    );
+    console.log('true:', trueIndex);
+    console.log('from: ', DragUpdate.source.index);
+    console.log('to: ', DragUpdate.destination.index);
+    if (trueIndex !== DragUpdate.source.index) {
+      console.log('! From: ', trueIndex);
+      console.log(trueIndex - (DragUpdate.source.index - DragUpdate.destination.index));
+    }
     if (DragUpdate.destination) {
-      this.props.reorderCollection({
-        from: DragUpdate.source.index,
-        to: DragUpdate.destination.index,
-      });
+      if (trueIndex !== DragUpdate.source.index) {
+        this.props.reorderCollection({
+          from: trueIndex,
+          to: trueIndex - (DragUpdate.source.index - DragUpdate.destination.index),
+        });
+      } else {
+        
+      }
     }
   };
 
@@ -55,9 +80,12 @@ class DraggableList extends Component {
 DraggableList.propTypes = {
   children: PropTypes.node,
   reorderCollection: PropTypes.func,
+  films: PropTypes.array,
 };
 
-const mapStateToProps = (state) => ({});
+const mapStateToProps = (state) => ({
+  films: state.collection.films,
+});
 
 const mapDispatchToProps = (dispatch) => ({
   reorderCollection: ({from, to}) => {
