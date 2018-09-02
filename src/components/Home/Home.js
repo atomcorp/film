3a0b5d879f21003144659afe76d466226e7353df
atomcorp <thomasmaxwellsmith@gmail.com>
2018-09-02 20:24:28 +0100

    Home page different for logged in

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,15 +1,30 @@
 import React from 'react';
+import {connect} from 'react-redux';
 import {Link} from 'react-router-dom';
+import {authType} from '@src/types';
 
-const Home = () => (
+const Home = ({auth}) => (
   <div>
     <h1>Home page</h1>
-    <Link to="/signin">Sign in</Link>
-    <br />
-    <Link to="/signup">Sign up</Link>
+    {auth.isAuth ? (
+      <Link to="/app">Your collection</Link>
+    ) : (
+      <div>
+        <Link to="/signin">Sign in</Link>
+        <br />
+        <Link to="/signup">Sign up</Link>
+      </div>
+    )}
+
     <br />
     <Link to="/c/-LIX6jO8zd0_6EV_ipdq">Example collection</Link>
   </div>
 );
 
-export default Home;
+Home.propTypes = {
+  auth: authType,
+};
+
+export default connect(({auth}) => ({
+  auth: auth,
+}))(Home);
