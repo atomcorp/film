5b5865bcdd3f0c2bf2da5663b91c3698b0f83421
atomcorp <thomasmaxwellsmith@gmail.com>
2018-09-04 21:39:44 +0100

    add draggable variable width sidebar

--- src/components/Collection/CollectionLayout.js
+++ src/components/Collection/CollectionLayout.js
@@ -3,17 +3,20 @@ import PropTypes from 'prop-types';
 import css from './CollectionLayout.css';
 import CollectionNameContainer from '../../containers/CollectionNameContainer';
 // import DraggableHandle from '@components/DraggableHandle/DraggableHandle';
-
+import SplitPane from 'react-split-pane';
+import './react-split-pane.css';
 // this can be used by editable and no-editable things
 const CollectionLayout = ({films, highlight, toggleWatched}) => (
   <div className={css.container}>
-    <div className={css.films}>
-      {toggleWatched}
-      <CollectionNameContainer />
-      <button>Add more movies button</button>
-      {films}
-    </div>
-    <div className={css.highlight}>{highlight}</div>
+    <SplitPane split="vertical" minSize="25%" maxSize="50%">
+      <div className={css.films}>
+        {toggleWatched}
+        <CollectionNameContainer />
+        <button>Add more movies button</button>
+        {films}
+      </div>
+      <div className={css.highlight}>{highlight}</div>
+    </SplitPane>
   </div>
 );
 
